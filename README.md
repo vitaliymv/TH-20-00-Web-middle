a42aee7a1582717781295882fb59138b299c2850
vitaliymv <movcan77@gmail.com>
2023-11-16 21:22:09 +0200

    Add function for search users github

---
+++ README.md
@@ -0,0 +1,58 @@
+# Hello
+## Hello
+*Hello*
+
+**Hello** *world*
+
+__Hello__
+
+_Hello_
+
+# ~~Hello~~
+
+***Test***
+
+H<sub>2</sub>O
+
+a=b<sup>2</sup>
+
+> # some text
+
+```html
+    <input type="text" placeholder="Username">
+```
+
+```javascript
+    let username = $(this).val();
+```
+
+```python   
+    print(a + b)
+```
+
+```java
+    System.out.println("Hello");
+```
+
+`#000000`
+
+[Github](https://github.com/)
+
+![GitHub](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM_TV2o6IKjWK5A2Ve3S3Wbdq-NpX-jy0NHP1uaokb&s)
+- Link -
++ Link +
+* Link 1
+
+* Link 2
+* Link 3
+    * fasf
+    * test
+        * test1
+            * test2
+
+1. test1
+1. test2
+1. test 3
+
+- [x] Convert 
+
---
+++ script.js
@@ -0,0 +1,28 @@
+$("#result").hide();
+
+$("input").on("change", function () {
+    let username = $(this).val();
+    if (username) {
+        $.ajax(`https://api.github.com/users/${username}`, {
+            success: function (result) {
+                let dateString = result.created_at;
+                let date = new Date(dateString);
+                $("#result").show();
+                $("#result").html(`
+                    <h3>${result.login}</h3>
+                    <img src="${result.avatar_url}">
+                    <a href="${result.html_url}" target="_blank">Profile</a>
+                    <p>
+                        ${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1).toString().padStart(2, "0")}-${date.getFullYear()}
+                    </p>
+                    <p>
+                    ${date.getHours().toString().padStart(2, "0")}:${(date.getMinutes()).toString().padStart(2, "0")}:${date.getSeconds().toString().padStart(2, "0")}
+                    </p>
+                `)
+            }
+        })
+    } else {
+        $("#result").empty();
+        $("#result").hide();
+    }
+})
